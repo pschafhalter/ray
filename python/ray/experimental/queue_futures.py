d278f16a74786e7a71978368c419581c39aa3c4c
Peter Schafhalter <pschafhalter@berkeley.edu>
2018-07-03 12:46:03 -0700

    Add queue implementation with futures

---
+++ python/ray/experimental/queue_futures.py
@@ -0,0 +1,125 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from collections import deque
+import numpy as np
+import time
+
+import ray
+
+
+class Empty(Exception):
+    pass
+
+
+class Full(Exception):
+    pass
+
+
+class Queue(object):
+    """Queue implementation on Ray.
+
+    Todo:
+        - not_empty
+        - not_full
+        - join
+        - mutex
+        - task_done
+        - unfinished_taskss
+    """
+    def __init__(self, maxsize=0):
+        self.maxsize = maxsize
+        self.actor = _QueueActor.remote(maxsize)
+
+    def qsize(self):
+        return ray.get(self.actor.qsize.remote())
+
+    def empty(self):
+        return ray.get(self.actor.qsize.remote())
+
+    def full(self):
+        return ray.get(self.actor.full.remote())
+
+    def put(self, item, block=True, timeout=None):
+        if self.maxsize <= 0:
+            self.actor.put.remote(item)
+        elif not block:
+            if not ray.get(self.actor.put.remote(item)):
+                raise Full
+        elif timeout is None:
+            # Polling
+            # TODO: implement with not_full condition variable
+            while not ray.get(self.actor.put.remote(item)):
+                # Consider adding time.sleep here
+                pass
+        elif timeout < 0:
+            raise ValueError("'timeout' must be a non-negative number")
+        else:
+            endtime = time.time() + timeout
+            # Polling
+            # TODO: implement with not_full condition variable
+            success = False
+            while not success and time.time() < endtime:
+                success = ray.get(self.actor.put.remote(item))
+            if not success:
+                raise Full
+
+    def get(self):
+        oid_bytes = np.random.bytes(20)
+        self.actor.get.remote(oid_bytes)
+        return ray.get(ray.ObjectID(oid_bytes))
+        
+    def put_nowait(self, item):
+        return self.put(block=False)
+
+    def get_nowait(self):
+        return self.get(block=False)
+
+
+@ray.remote
+class _QueueActor(object):
+    def __init__(self, maxsize):
+        self.maxsize = maxsize
+        self._init(maxsize)
+
+    def qsize(self):
+        return self._qsize()
+
+    def empty(self):
+        return not self._qsize()
+
+    def full(self):
+        return 0 < self.maxsize <= self._qsize()
+
+    def put(self, item):
+        if self.maxsize > 0 and self._qsize() >= self.maxsize:
+            return False
+        if len(self.waiting):
+            oid = self.waiting.popleft()
+            ray.worker.global_worker.store_and_register(oid, item)
+        else:
+            self._put(item)
+        return True
+
+    def get(self, oid_bytes):
+        oid = ray.ObjectID(oid_bytes)
+        if self._qsize():
+            item = self._get()
+            ray.worker.global_worker.store_and_register(oid, item)
+        else:
+            self.waiting.append(ray.ObjectID(oid_bytes))
+
+    # Override these for different queue implementations
+    def _init(self, maxsize):
+        self.queue = deque()
+        self.waiting = deque()
+
+    def _qsize(self):
+        return len(self.queue)
+
+    def _put(self, item):
+        self.queue.append(item)
+
+    def _get(self):
+        return self.queue.popleft()
